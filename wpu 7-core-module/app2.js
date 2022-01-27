de1d23c72a34903a813f5b8155d6f2d82cec1e76
reikusumah <kusumah.rei@gmail.com>
2022-01-27 17:19:41 +0700

    Add and refactoring Contact-App from WPU 7

---
+++ wpu 7-core-module/app2.js
@@ -0,0 +1,84 @@
+// hasil refactoring dari pertemuan 7
+// memperbaiki kemungkinan "CALLBACK HELL"
+// pada pertanyaan
+// maka digunakanlah async-await
+
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+const dataPath = "./data/contacts.json";
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, "[]", "utf-8");
+}
+
+// bungkus pertanyaan ke dalam Promise untuk
+// async-await
+const tulisPertanyaan = (pertanyaan) => {
+  return new Promise((resolve, reject) => {
+    rl.question(pertanyaan, (jawaban) => {
+      resolve(jawaban);
+    });
+  });
+};
+
+// const pertanyaan1 = () => {
+//   return new Promise((resolve, reject) => {
+//     rl.question("Masukkan nama kamu : ", (nama) => {
+//       resolve(nama);
+//     });
+//   });
+// };
+
+// const pertanyaan2 = () => {
+//   return new Promise((resolve, reject) => {
+//     rl.question("Masukkan email kamu : ", (email) => {
+//       resolve(email);
+//     });
+//   });
+// };
+
+const main = async () => {
+  const nama = await tulisPertanyaan("Masukkan nama anda : ");
+  const email = await tulisPertanyaan("Masukkan email anda : ");
+  const noHP = await tulisPertanyaan("Masukkan nomor hanphone anda : ");
+
+  const contact = { nama, email, noHP };
+  const file = fs.readFileSync("data/contacts.json", "utf-8");
+  const contacts = JSON.parse(file);
+
+  contacts.push(contact);
+
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+  console.log("Terima kasih sudah memasukkan data.");
+
+  rl.close();
+};
+
+main();
+
+// rl.question("Masukkan nama kamu : ", (nama) => {
+//   rl.question("Masukkan No. Hp kamu : ", (noHp) => {
+//     const contact = { nama, noHp };
+//     const file = fs.readFileSync("data/contacts.json", "utf-8");
+//     const contacts = JSON.parse(file);
+
+//     contacts.push(contact);
+
+//     fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+//     console.log("Terima kasih sudah memasukkan data.");
+
+//     rl.close();
+//   });
+// });
